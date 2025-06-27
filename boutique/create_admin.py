24010b3e545e62d9631ed4146b1a574486c6f6e6
Sorycamara <masocietevente3174@gmail.com>
2025-06-28 00:59:00 +0200

    Fix chemin et initialisation Django pour create_admin

--- boutique/create_admin.py
+++ boutique/create_admin.py
@@ -1,7 +1,9 @@
+import sys
 import os
 import django
 
 # Initialise Django
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monprojet.settings')
 django.setup()
 
